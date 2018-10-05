35bcbf4c41ea2b909e1f4d90ac92316cc93677b1
Anna Erkers <31205429+Hilwynn@users.noreply.github.com>
2018-10-05 15:12:37 +0200

    Basic styling and content added to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react"
+import LandingPage from "./Pages/LandingPage"
 import "./App.css"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello world</h1>
+        <h1>Maybe this is header?</h1>
+        <div className="page-container">
+          <LandingPage />
+        </div>
+        <h1>Yes this is footer</h1>
       </div>
     )
   }
