3c32004f153c2d9da2bec016e05a4ce9494063f8
Anna Erkers <31205429+Hilwynn@users.noreply.github.com>
2018-10-08 15:52:22 +0200

    Added fetch function to the CasePage component

--- src/Pages/CasePage/index.js
+++ src/Pages/CasePage/index.js
@@ -1,11 +1,30 @@
 import React, { Component } from "react"
+import { Link } from "react-router-dom"
 import "./style.css"
 
 export default class CasePage extends Component {
+  state = {
+    project: {}
+  }
+
+  componentDidMount() {
+    const { path } = this.props.match.params
+    const apiUrl = `/json/${path}.json`
+    fetch(apiUrl).then(response => response.json()).then(json => {
+      this.setState({
+        project: json
+      })
+    })
+  }
+
   render() {
+    const { project } = this.state
+    console.log(project)
+
     return (
       <div>
-        <h2>Link back</h2>
+        <Link to="/">&larr; Back</Link>
+        <p>{project.title}</p>
 
       </div>
     )
