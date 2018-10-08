ebb764412642f3bc3544def54077c603ca2f8697
Anna Erkers <31205429+Hilwynn@users.noreply.github.com>
2018-10-08 15:51:44 +0200

    Updated routing in App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react"
-import { Switch, Route } from "react-router-dom"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import LandingPage from "./Pages/LandingPage"
 import CasePage from "./Pages/CasePage"
 import FourOhFourPage from "./Pages/FourOhFourPage"
@@ -9,16 +9,18 @@ import "./App.css"
 export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="page-container">
-          <Switch>
-            <Route exact path="/" component={LandingPage} />
-            <Route path="/case/:path" component={CasePage} />
-            <Route component={FourOhFourPage} />
-          </Switch>
+      <Router>
+        <div className="App">
+          <div className="page-container">
+            <Switch>
+              <Route exact path="/" component={LandingPage} />
+              <Route exact path="/case/:path" component={CasePage} />
+              <Route component={FourOhFourPage} />
+            </Switch>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>
     )
   }
 }
