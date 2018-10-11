a028cc7bc8a3a9f8526f8d09af119b4c9c6152d2
Anna Erkers <31205429+Hilwynn@users.noreply.github.com>
2018-10-11 10:38:22 +0200

    Changed to HashRouter for deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { HashRouter as Router, Switch, Route } from "react-router-dom"
 import LandingPage from "./Pages/LandingPage"
 import CasePage from "./Pages/CasePage"
 import FourOhFourPage from "./Pages/FourOhFourPage"
