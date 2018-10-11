7d39384fbb01a3a8d81c3bd5d5c36d3adf7f1019
Anna Erkers <31205429+Hilwynn@users.noreply.github.com>
2018-10-11 10:36:03 +0200

    Updates

---
+++ precache-manifest.589b471f201595707994dc1076704d3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "268d8efc7e069f86d4e8",
+    "url": "/personal-portfolio-technigo/static/js/runtime~main.268d8efc.js"
+  },
+  {
+    "revision": "b6fb45e8d91dbddcbf74",
+    "url": "/personal-portfolio-technigo/static/js/main.b6fb45e8.chunk.js"
+  },
+  {
+    "revision": "5926706c483410cb65a8",
+    "url": "/personal-portfolio-technigo/static/js/1.5926706c.chunk.js"
+  },
+  {
+    "revision": "b6fb45e8d91dbddcbf74",
+    "url": "/personal-portfolio-technigo/static/css/main.4ff41870.chunk.css"
+  },
+  {
+    "revision": "9cddaf68a531fc84204c0cc7eaf081c3",
+    "url": "/personal-portfolio-technigo/index.html"
+  }
+];
