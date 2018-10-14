ffc98ce9dee19dea54564ee6464b73685f9e9a50
Anna Erkers <31205429+Hilwynn@users.noreply.github.com>
2018-10-14 09:18:27 +0200

    Changed HashRouter to BrowserRouter in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react"
-import { HashRouter as Router, Switch, Route } from "react-router-dom"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import LandingPage from "./Pages/LandingPage"
 import CasePage from "./Pages/CasePage"
 import FourOhFourPage from "./Pages/FourOhFourPage"
